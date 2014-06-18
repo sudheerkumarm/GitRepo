0ce36ddedb68fc3ccd07362fa97c92b5285fbf6b
sudheerkumarm <sudheerkumarm@gmail.com>
2014-06-18 13:34:09 +0530

    build1 deploy1 test1

--- src/main/java/com/test/app/App.java
+++ src/main/java/com/test/app/App.java
@@ -8,10 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
         System.out.println( "This is next line for testing" );
 	System.out.println( "I  am testing1" );
-	System.out.println( "I  am testing2" )
 	System.out.println(" This is other line for auto build");
     }
 }
