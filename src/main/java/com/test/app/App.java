43a3246b2cb5c4e0181380ca9cfa3d8f8bd186ef
sudheerkumarm <sudheerkumarm@gmail.com>
2014-06-18 15:43:05 +0530

    testing app

--- src/main/java/com/test/app/App.java
+++ src/main/java/com/test/app/App.java
@@ -8,11 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "This is next line for testing" );
-	System.out.println( "I  am testing1" );
-	System.out.println(" This is other line for auto build");
-	System.out.println(" This is other line for auto build111");
-System.out.println(" This is other line for auto check 3333 ");
+        System.out.println(" Build testing ");
 
     }
 }
